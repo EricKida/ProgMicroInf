058d6eeecc97f7b12062094e564d7e7c57687998
Eric Kida <51220926+EricKida@users.noreply.github.com>
2021-06-16 13:36:07 -0300

    Aula 1
    Exemplos e Atividades

---
+++ Aula1/Atividade1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Atividades1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inicio
+            //Ex. 1
+            /*string nome; ;
+            Console.Write("Digite o nome do usuário: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Nome do usuário: "+nome);
+            Console.ReadKey();*/
+
+            //Ex. 2
+            /*double n1, n2, adicao, subtracao, divisao, multiplicacao;
+            Console.Write("Digite o primeiro número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            adicao = n1 + n2;
+            subtracao = n1 - n2;
+            divisao = n1 / n2; 
+            multiplicacao = n1 * n2;
+            Console.WriteLine("================================");
+            Console.WriteLine(n1 + "+" + n2 + " é: " + adicao);
+            Console.WriteLine(n1 + "-" + n2 + " é: " + subtracao);
+            Console.WriteLine(n1 + "/" + n2 + " é: " + divisao);
+            Console.WriteLine(n1 + "*" + n2 + " é: " + multiplicacao);
+            Console.ReadKey();*/
+
+            //Ex. 3
+            /*string nome;
+            double ano, ano_atual, idade;
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Seu ano de nascimento: ");
+            ano = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Em que ano estamos? ");
+            ano_atual = Convert.ToDouble(Console.ReadLine());
+            idade = ano_atual - ano;
+            Console.WriteLine("A idade de "+nome+" até o final de "+ano_atual+" é de "+idade+ " anos");
+            Console.ReadKey();*/
+
+            //Ex. 4
+            /*double n1, n2, n3, r;
+            Console.Write("Digite o primeiro número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o terceiro número: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+            r = n1 + n2 + n3;
+            r = r / 3;
+            Console.WriteLine("A média aritmética de "+n1+ ", " + n2 + " e " + n3 + " é de: " + r);
+            Console.ReadKey(); */
+
+            //Ex. 5
+            /*double n1, r;
+            Console.Write("Digite um número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            r = n1 - 1;
+            Console.WriteLine("O valor antecessor de "+n1+" é "+r);
+            Console.ReadKey();*/
+
+            //Ex. 6
+            /*double b, h, a;
+            Console.Write("Digite a base do Retângulo: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a altura do Retângulo: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            a = b * h;
+            Console.WriteLine("A área do retângulo é de: " + a);
+            Console.ReadKey();*/
+
+            //Ex. 7 - não sei ainda
+            /*double data, idade, dia, mes, ano, r;
+            string nome;
+            Console.Write("Nome do usuário: ");
+            nome = Console.ReadLine();
+            Console.Write("Idade do "+nome);
+            data = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("A base do retângulo é: " + a);
+            Console.ReadKey();*/
+
+            //Ex. 8
+            /*double n1, n2, n3, mf1, mf2, mf3, m, mf;
+            string aluno;
+            Console.Write("Nome do aluno: ");
+            aluno = Console.ReadLine();
+            Console.Write("Nota 1 (Peso 2) do "+aluno+": ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            mf1 = n1 * 2;
+            Console.Write("Nota 1 (Peso 3) do " + aluno + ": ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            mf2 = n2 * 3;
+            Console.Write("Nota 1 (Peso 5) do " + aluno + ": ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+            mf3 = n3 * 5;
+            m = mf1 + mf2 + mf3;
+            mf = m / 10;
+            Console.WriteLine("A média final de " + aluno + " é de "+mf);
+            Console.ReadKey();*/
+            //fim
+        }
+    }
+}
---
+++ Aula1/Exemplos1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace proj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inicio
+            //#Desenvolvedor: Eric Kida
+            //Criado:15.06.2021
+
+            //EXEMPLO 1 - double irá usar o número, então irá retornar 12
+            /*double a, b, c;
+            a = 10;
+            b = 2;
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();*/
+
+            //EXEMPLO 2 - String usa o número como texto, então irá retornar 102
+            /*string a, b, c;
+            a = "10";
+            b = "2";
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();*/
+
+            //EXEMPLO 3 - Adição de texto no resultado final utilizando double
+            /*double a, b, c;
+            a = 10;
+            b = 2;
+            c = a + b;
+            Console.WriteLine("O resultado é: "+c);
+            Console.ReadKey();*/
+
+            //EXEMPLO 4 - Dando valor a string com nome e nome da instituição e retornando na exibição
+            /*string nome1, nome2;
+            nome1 = "ERIC KIDA";
+            nome2 = "FATEC";
+            Console.WriteLine("Bem vindo "+nome1+" à "+nome2+" PG");
+            Console.ReadKey();*/
+
+            //EXEMPLO 5 - Usuário insere valores N1 e N2 e é realizada uma soma(operação) entre eles e após isso, é exibida.
+            /*double n1, n2, r;
+            Console.Write("Digite o primeiro número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            r = n1 + n2;
+            Console.WriteLine("============================");
+            Console.WriteLine("A soma de N1+N2 é: " + r);
+            Console.WriteLine("============================");
+            Console.ReadKey();*/
+
+            //EXEMPLO 5 - Usuário insere valores N1 e N2 e é realizada uma soma(operação) entre eles e após isso, é exibida.
+            /*double n1, n2, r;
+            Console.Write("Digite o primeiro número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            r = n1 + n2;
+            Console.WriteLine("============================");
+            Console.WriteLine("A soma de N1+N2 é: " + r);
+            Console.WriteLine("============================");
+            Console.ReadKey();*/
+            //fim
+        }
+    }
+}
